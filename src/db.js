70bd10fa227ab9858e8de1ceee246a6372a71ae1
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-02-10 17:31:56 +0000

    Some temporary stuff for uploading to firestore

---
+++ src/db.js
@@ -0,0 +1,55 @@
+const csvParser = require('./utils/csvParser');
+
+class Query {
+  constructor(period, currency, referenceCurrency, salary) {
+    this.period = period;
+    this.currency = currency;
+    this.referenceCurrency = referenceCurrency;
+    this.salary = salary;
+  }
+}
+
+const calculateRate = (currency, referenceCurrency, totalAmount) =>
+  (totalAmount || 1) * (referenceCurrency / currency);
+
+const fetchRates = query => new Promise((resolve, reject) => {
+  csvParser('./data/eurofxref-hist.csv')
+    .then(exchangeRates =>
+      resolve(exchangeRates
+        .filter(rate => rate.Date >= query.period)
+        .map(rate => (
+          {
+            date: rate.Date,
+            currency: query.currency,
+            referenceCurrency: query.referenceCurrency,
+            exchangeRate: calculateRate(rate[query.currency], rate[query.referenceCurrency]),
+            salaryExchangeRate: query.salary &&
+              calculateRate(rate[query.currency], rate[query.referenceCurrency], query.salary),
+          }
+        ))))
+    .catch(err => reject(err));
+
+  /* csvParser('./data/eurofxref-hist.csv')
+    .then(exchangeRates =>
+      resolve(exchangeRates
+        .filter(rate => rate.Date >= query.period)
+        .map(rate => (
+          {
+            date: rate.Date,
+            currency: query.currency,
+            referenceCurrency: query.referenceCurrency,
+            exchangeRate: calculateRate(rate[query.currency], rate[query.referenceCurrency]),
+            salaryExchangeRate:
+              calculateRate(rate[query.currency], rate[query.referenceCurrency], query.salary),
+          }
+        ))))
+    .catch(err => reject(err)); */
+});
+
+const db = {
+  fetchRates,
+  Query,
+};
+
+module.exports = db;
+
