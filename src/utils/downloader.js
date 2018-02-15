fe59f314023cf197eb871e6ed4c0c31269b7208e
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-02-15 17:09:51 +0000

    Add schedule to fetch historical rates at 16:00 every weekday

--- src/utils/downloader.js
+++ src/utils/downloader.js
@@ -3,14 +3,19 @@ const https = require('https');
 const cheerio = require('cheerio');
 const request = require('request');
 const unzip = require('unzip');
+const schedule = require('node-schedule');
 
 const fetchHref = (selector, resolve, reject) => {
   const url = 'https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html';
   request({
     uri: url,
   }, (error, response, body) => {
-    if (!response) {
+    if (error) {
       reject(error.message);
+      return;
+    } else if (response.statusCode !== 200) {
+      reject(response.message);
+      return;
     }
     const $ = cheerio.load(body);
 
@@ -78,7 +83,29 @@ const fetchECBRates = hrefFunc =>
     .then(url => download(url))
     .then(stream => unpackage(stream));
 
+let jobSchedule;
+const scheduleJob = () => {
+  const rule = new schedule.RecurrenceRule();
+  rule.dayOfWeek = [0, new schedule.Range(0, 4)];
+  rule.hour = 16;
+  rule.minute = 0;
+
+  jobSchedule = schedule.scheduleJob(rule, () => {
+    fetchECBRates(fetchHistoricalRatesHref);
+  });
+
+  return jobSchedule;
+};
+
+const cancelScheduledJob = () => {
+  if (jobSchedule) {
+    jobSchedule.cancel();
+  }
+};
+
 const cli = (args) => {
+  let ret;
+
   (args || process.argv).forEach((argv) => {
     switch (argv) {
       case 'daily':
@@ -91,9 +118,17 @@ const cli = (args) => {
         fetchECBRates(fetchCurrentRatesHref);
         fetchECBRates(fetchHistoricalRatesHref);
         break;
+      case 'schedule':
+        ret = scheduleJob();
+        break;
+      case 'stop-schedule':
+        cancelScheduledJob();
+        break;
       default:
     }
   });
+
+  return ret;
 };
 
 const downloader = {
--- test/downloader.test.js
+++ test/downloader.test.js
@@ -3,6 +3,7 @@ const mockfs = require('mock-fs');
 const sinon = require('sinon');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const nock = require('nock');
 
 const {
   downloader, cli, __set__, __get__,
@@ -19,11 +20,36 @@ describe('downloader', () => {
     });
   });
 
+
+  it('should expose two methods', () => {
+    expect(downloader.fetchCurrentRates).to.be.instanceOf(Function);
+    expect(downloader.fetchHistoricalRates).to.be.instanceOf(Function);
+  });
+
   it('should fetch the current rates from the European Central Bank and return the path', () =>
     expect(downloader.fetchCurrentRates()).to.eventually.equal('./data/eurofxref.csv')).timeout(5000);
 
   it('should fetch the historical rates from the European Central Bank and return the path', () =>
     expect(downloader.fetchHistoricalRates()).to.eventually.equal('./data/eurofxref-hist.csv')).timeout(5000);
+
+  describe('error handling', () => {
+    it('should fail as expected when ECB site is down', () => {
+      nock('https://www.ecb.europa.eu')
+        .get('/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html')
+        .reply(404);
+
+      return expect(downloader.fetchHistoricalRates()).to.eventually.be.rejected;
+    });
+
+    it('should fail as expected when the url passed to download can not be resolved', () => {
+      const download = __get__('download');
+      nock('www.example.com')
+        .get('/thefile.zip')
+        .reply(404);
+
+      return expect(download('www.example.com/thefile.zip')).to.eventually.be.rejected;
+    });
+  });
 
   describe('cli', () => {
     let stub;
@@ -52,6 +78,29 @@ describe('downloader', () => {
       cli(['historical']);
       expect(stub.callCount).to.equal(1);
       expect(stub.getCall(0).args[0]).to.equal(__get__('fetchHistoricalRatesHref'));
+    });
+
+    it('should schedule a job to fetch historical rates at 16:00 every weekday', () => {
+      const clock = sinon.useFakeTimers(new Date('2018-02-15T15:59:59'));
+      cli(['schedule']);
+      expect(stub.callCount).to.equal(0);
+      clock.tick(1000);
+      expect(stub.callCount).to.equal(1);
+      clock.restore();
+    });
+
+    it('should be possible to cancel a scheduled job', () => {
+      const schedule = cli(['schedule']);
+      const spy = sinon.spy(schedule, 'cancel');
+
+      expect(spy.callCount).to.equal(0);
+      cli(['stop-schedule']);
+      expect(spy.callCount).to.equal(1);
+    });
+
+    it('should do nothing if an unknown argument is passed', () => {
+      cli(['foobarbaz']);
+      expect(stub.callCount).to.equal(0);
     });
 
     afterEach(() => {
