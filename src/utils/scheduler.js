204ae1e53785d87e7553875b91ce7590fb46c7d5
Fredrik Mäkilä <fredrik.makila@gmail.com>
2018-03-29 17:26:23 +0100

    Method for fetching initial data if it does not exist

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -1,9 +1,37 @@
 const { CronJob } = require('cron');
-const downloader = require('./downloader');
-const logger = require('./logger');
 const moment = require('moment');
+const fs = require('fs');
+const path = require('path');
+const { downloader } = require('./downloader');
+const logger = require('./logger');
+
+/**
+ * Downloads initial data if no data exists
+ */
+const initialDataDownload = () => {
+  const dirPath = [__dirname, '..', '..', 'data'];
+  const filePath = [...dirPath, 'eurofxref-hist.csv'];
+
+  const jsonFilePath = path.join(...filePath);
+  fs.access(jsonFilePath, (err) => {
+    if (err) {
+      logger.warn(err);
+      logger.info(`No exchange rate data exists at ${JSON.stringify(jsonFilePath)}`);
+      const jsonDirPath = path.join(...dirPath);
+      fs.access(jsonDirPath, (dirErr) => {
+        if (dirErr) {
+          logger.info(`Creating directory ${JSON.stringify(jsonDirPath)}`);
+          fs.mkdirSync(jsonDirPath);
+        }
+        downloader.fetchHistoricalRates();
+      });
+    }
+  });
+};
 
 const scheduleJob = () => {
+  initialDataDownload();
+
   const job = new CronJob({
     cronTime: '0 16 * * 1-5',
     onTick: async () => {
